4164118871577747404b4675ca5ff67971f9ff25
Abdelrahman <abdulrahmanaladdin1@gmail.com>
2025-10-01 11:22:54 +0300

    Finished the three charts

---
+++ TaskManagement/Models/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagement.Models
+{
+    public class ChartAttributes
+    {
+        public string DataUrl { get; set; }
+        public string ChartTitle { get; set; }
+        public string XAxisTitle { get; set; }
+        public string YAxisTitle { get; set; }
+        public string ChartId { get; set; }
+    }
+}
