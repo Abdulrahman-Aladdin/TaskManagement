0950bf912c3f24245e21b8755fe4e84f87ae0dfb
JanaElnagar <janaelnagar18@gmail.com>
2025-10-01 12:07:34 +0300

    Completed Hours BarChart

---
+++ TaskManagement/Models/TaskStatusChartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagement.Models
+{
+    public class TaskStatusChartViewModel
+    {
+        public string Status { get; set; }
+        public int Count { get; set; }
+    }
+}
